08510e54788026f0c19bde512435b82eb63cbd8c
Sudhanva Sreesha <sudhanvas@gmail.com>
2015-09-12 02:30:38 -0400

    Added a toolbar and a FramLayout to use fragments

--- app/src/main/java/mhacks/openmusic/MainActivity.java
+++ app/src/main/java/mhacks/openmusic/MainActivity.java
@@ -1,13 +1,21 @@
 package mhacks.openmusic;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.activity_main)
+@EActivity(R.layout.main_activity)
 public class MainActivity extends AppCompatActivity {
 
+	@ViewById(android.R.id.custom)
+	Toolbar mToolbar;
+
+	@AfterViews
+	public void onViewChanged() {
+		setSupportActionBar(mToolbar);
+		setTitle("Song");
+	}
 }
